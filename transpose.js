bdf4a4a586b437f14ab60ae27eab01952dd85e3a
Evander Owusu <owu.evander@gmail.com>
2021-12-02 01:04:03 +0000

    Peer-programming wordsearch

---
+++ transpose.js
@@ -0,0 +1,16 @@
+// const transpose = function (matrix) {
+//   // result
+//   let result = [];
+//   let columnNumber = matrix[0].length;
+
+//   for (let j = 0; j < columnNumber; j++) {
+//     let newRow = [];
+//     for (let i = 0; i < matrix.length; i++) {
+//       newRow.push(matrix[i][j]);
+//     }
+//     result.push(newRow);
+//   }
+//   return result;
+// };
+
+// module.exports = transpose;
--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,39 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const transpose = function (matrix) {
+  // result
+  let result = [];
+  let columnNumber = matrix[0].length;
 
-module.exports = wordSearch
+  for (let j = 0; j < columnNumber; j++) {
+    let newRow = [];
+    for (let i = 0; i < matrix.length; i++) {
+      newRow.push(matrix[i][j]);
+    }
+    result.push(newRow);
+  }
+  return result;
+};
+
+const wordSearch = (letters, word) => {
+  if (letters.length < 1) {
+    return undefined;
+  }
+
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  tempTranspose = transpose(letters);
+  const verticalJoin = tempTranspose.map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
